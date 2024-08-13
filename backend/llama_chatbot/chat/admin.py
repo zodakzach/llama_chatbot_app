783da4120cb109ccc3deb0990d0d36def3ae61d7
zodakzach <zachary.cervenka@gmail.com>
2024-08-13 15:29:18 -0400

    add tests for chat

--- backend/llama_chatbot/chat/admin.py
+++ backend/llama_chatbot/chat/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ChatThread, ChatMessage
 
-# Register your models here.
+class ChatMessageInline(admin.TabularInline):
+    model = ChatMessage
+    extra = 1  # Number of empty forms to display
+
+class ChatThreadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title', 'created_at', 'updated_at')
+    search_fields = ('user__username', 'title')
+    inlines = [ChatMessageInline]
+
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('thread', 'sender', 'content', 'created_at')
+    list_filter = ('sender', 'created_at')
+    search_fields = ('content',)
+
+admin.site.register(ChatThread, ChatThreadAdmin)
+admin.site.register(ChatMessage, ChatMessageAdmin)
