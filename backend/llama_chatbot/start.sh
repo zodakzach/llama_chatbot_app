d6dba4e28d702f8d9349d05f1ae9fc55df4633fb
zodakzach <zachary.cervenka@gmail.com>
2024-09-01 11:46:45 -0400

    docker compose change

--- backend/llama_chatbot/start.sh
+++ backend/llama_chatbot/start.sh
@@ -1,8 +1,2 @@
-# Start Memcached
-memcached -u memcache &
-# Wait for Memcached to start
-sleep 5
-# Start Ollama server
-ollama serve &
 # Start Django server
 python manage.py runserver 0.0.0.0:8000
